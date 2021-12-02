e38653fbaadb9f98fd71c3526ec5ae0e5f4bc4f1
JagTheFriend <jagthefriend12@gmail.com>
2021-12-02 14:05:31 +0200

    Updated file

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,15 +1,31 @@
 import { NextFunction, Request, Response } from 'express';
+import appModel from '@models/app.model';
 
 class AppController {
-  public index = (req: Request, res: Response, next: NextFunction) => {
+  public index = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      res.render('index');
+      const urls = await appModel.find({});
+      res.render('index', { shortUrls: urls });
     } catch (error) {
       next(error);
     }
   };
   public shortUrl = async (req: Request, res: Response, next: NextFunction) => {
     try {
+      await appModel.create({ full: req.body.fullUrl });
+      res.redirect('/');
+    } catch (error) {
+      next(error);
+    }
+  };
+  public viewShortUrl = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const url = req.params.shortUrl;
+      const shortUrl = await appModel.findOne({ short: url });
+      if (shortUrl == null) return res.sendStatus(404);
+      shortUrl.clicks++;
+      shortUrl.save();
+      res.redirect(shortUrl.full);
     } catch (error) {
       next(error);
     }
