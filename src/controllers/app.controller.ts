64f4292d8c90db5ce173c1a07814600f765dcc27
JagTheFriend <jagthefriend12@gmail.com>
2021-12-02 13:20:49 +0200

    The index.ejs file can now be sent to the client

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 class AppController {
   public index = (req: Request, res: Response, next: NextFunction) => {
     try {
-      res.sendStatus(200);
+      res.render('index');
     } catch (error) {
       next(error);
     }
